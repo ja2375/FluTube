7158635a4c07b17242b8015366a13323e52259b7
Javi Hurtado <javier.hurtado@edelvives.es>
2019-11-12 19:08:07 +0100

    Migrate example project to AndroidX

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ja2375/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ja2375/Desktop/projs_flutter/plugins/FluTube/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ja2375/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- lib/src/flutube_player.dart
+++ lib/src/flutube_player.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/services.dart';
